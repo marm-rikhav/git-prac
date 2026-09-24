8a39dd9b555e294542f36cca521d184c7422255c
marm <marm.rikhav@gmail.com>
2026-09-24 10:27:30 +0530

    New files

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      reportsDirectory: './coverage',
+      include: ['src/**/*.{js,jsx}'],
+      exclude: ['src/main.jsx', 'src/test/**'],
+    },
+  },
   server: {
     port: 3000,
   }
--- server/index.js
+++ server/index.js
@@ -38,4 +38,8 @@ const startServer = async () => {
   }
 };
 
-startServer();
+if (require.main === module) {
+  startServer();
+}
+
+module.exports = { app, startServer };
